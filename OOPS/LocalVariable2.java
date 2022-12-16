8c92575a2045d876f6baf92c7a56f475f9ebdc1c
rutvik17-Git <95428826+rutvik17-Git@users.noreply.github.com>
2022-12-16 19:18:03 +0530

    Add files via upload

---
+++ OOPS/LocalVariable2.java
@@ -0,0 +1,17 @@
+package bitsanspieces;
+
+public class LocalVariable2 {
+
+	public static void main(String[] args) {
+		
+		
+			for(int x=1;x<=5;x++)
+			System.out.println(x);
+			
+			
+		
+		//System.out.println(x);
+
+	}
+
+}
