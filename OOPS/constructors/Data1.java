43a98f93b10923daebb0e2dc408804ec5754a9f8
rutvik17-Git <95428826+rutvik17-Git@users.noreply.github.com>
2022-12-16 19:55:07 +0530

    Add files via upload

---
+++ OOPS/constructors/Data1.java
@@ -0,0 +1,17 @@
+package constructors;
+
+public class Data1 {
+	
+	int a,b,c;
+	
+	Data1(int pa,int pb,int pc){
+		
+		a=pa;
+		b=pb;
+		c=pc;
+	}
+
+	void showAbc() {
+		System.out.println("a = "+a+" b ="+b+" c ="+c);
+	}
+}
---
+++ OOPS/constructors/Test.java
@@ -0,0 +1,13 @@
+package constructors;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Data1 d1=new Data1(100,200,300);
+		
+		d1.showAbc();
+	}
+
+}
